61174b3e2dce52dec31e5029ec9927d7b82cafb1
Greatest4Ce <108977645+Greatest4Ce@users.noreply.github.com>
2024-03-19 19:18:22 +0300

    Initial Commit

---
+++ taskbook/taskbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  taskbookApp.swift
+//  taskbook
+//
+//  Created by Карим Султанбеков on 19.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct taskbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
